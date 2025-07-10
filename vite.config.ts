56f4bd5f2604ca88d1f857d0408fec921d03a437
delo0055 <tristan.deloeil@etudiant.univ-reims.fr>
2025-07-10 10:56:40 +0200

    ADD: Add alias $lib

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import tailwindcss from "@tailwindcss/vite";
 import { visualizer } from "rollup-plugin-visualizer";
+import path from "path";
 // import { crx } from "@crxjs/vite-plugin";
 // import manifest from "./manifest.json";
 
@@ -17,6 +18,18 @@ export default defineConfig({
     }),
     visualizer({}),
   ],
+  resolve: {
+    alias: {
+      // $components: path.resolve(__dirname, "./src/lib/components"),
+      // $stores: "/src/lib/stores",
+      // $api: "/src/api",
+      // $hooks: path.resolve(__dirname, "./src/lib/hooks"),
+      // $utils: "/src/lib/utils",
+      // $classes: "/src/lib/classes",
+      $lib: path.resolve(__dirname, "src/lib"),
+      $components: path.resolve(__dirname, "src/lib/components"),
+    },
+  },
   build: {
     rollupOptions: {
       output: {
